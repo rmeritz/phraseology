2414438c460ed45496e7841df78fdc5ad1d4d936
Rebecca Meritz <rebecca@meritz.com>
2012-06-09 00:05:40 +0200

    fix validates presence

--- app/models/word.rb
+++ app/models/word.rb
@@ -11,12 +11,12 @@ class Word < Neo4j::Rails::Model
   property :worked_poorly_as_last_word, :default => 0, :type => :integer
 
   validates :word, :presence => true
-  validates :found_in_text_as_first_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
-  validates :worked_as_first_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
-  validates :worked_poorly_as_first_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
-  validates :found_in_text_as_last_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
-  validates :worked_as_last_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
-  validates :worked_poorly_as_last_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :found_in_text_as_first_word, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_as_first_word, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_poorly_as_first_word, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :found_in_text_as_last_word, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_as_last_word, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_poorly_as_last_word, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
 
   has_n(:word).relationship(WordOrder)
 
--- app/models/word_order.rb
+++ app/models/word_order.rb
@@ -4,8 +4,8 @@ class WordOrder < Neo4j::Rails::Relationship
   property :worked_in_text_well, :default => 0, :type => :integer
   property :worked_in_text_poorly, :default => 0, :type => :integer
 
-  validates :found_in_text :worked_in_text_well \
-            :worked_in_text_poorly, :presense => true, \
-            :numericality => {:greater_than_or_equal_to => 0}
+  validates :found_in_text, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_in_text_well, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_in_text_poorly, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
 
 end
