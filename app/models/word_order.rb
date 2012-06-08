feac0b58dd43dfb1fb2cc305981a13b3c709f2ee
Rebecca Meritz <rebecca@meritz.com>
2012-06-09 00:26:53 +0200

    Add fixtures

--- app/models/word_order.rb
+++ app/models/word_order.rb
@@ -1,10 +1,10 @@
 class WordOrder < Neo4j::Rails::Relationship
 
-  property :found_in_text, :default => 0, :type => :integer
+  property :found_in_text, :default => 1, :type => :integer
   property :worked_in_text_well, :default => 0, :type => :integer
   property :worked_in_text_poorly, :default => 0, :type => :integer
 
-  validates :found_in_text, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :found_in_text, :presence => true, :numericality => {:greater_than_or_equal_to => 1, :only_integer => true}
   validates :worked_in_text_well, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
   validates :worked_in_text_poorly, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
 
