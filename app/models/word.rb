9b1c0c4ea11492995bc77047b8207c677c82ee00
Rebecca Meritz <rebecca@meritz.com>
2012-06-08 22:38:56 +0200

    Trying to get Word class to compile

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < Neo4j::Rails::Model
-
+  index :word
   property :word, :type => :string
 
   property :found_in_text_as_first_word, :default => 0, :type => :integer
@@ -10,18 +10,14 @@ class Word < Neo4j::Rails::Model
   property :worked_as_last_word, :default => 0, :type => :integer
   property :worked_poorly_as_last_word, :default => 0, :type => :integer
 
-  index :word
+  validates :word, :presences => true, :uniqueness => { :case_sensitive => false }
+  validates :found_in_text_as_first_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_as_first_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_poorly_as_first_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :found_in_text_as_last_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_as_last_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
+  validates :worked_poorly_as_last_word, :presense => true, :numericality => {:greater_than_or_equal_to => 0, :only_integer => true}
 
-  validates :word, :presense => true, \
-                   :uniqueness => { :case_sensitive => false }
-
-  validates :found_in_text_as_first_word :worked_as_first_word \
-            :worked_poorly_as_first_word :found_in_text_as_last_word \
-            :worked_as_last_word :worked_poorly_as_last_word, \
-            :presense => true, \
-            :numericality => {:greater_than_or_equal_to => 0}
-
-  has_n(:comes_before).to(Word).relationship(WordOrder)
-  has_n(:comes_after).from(Word).relationship(WordOrder)
+  has_n(:word).relationship(WordOrder)
 
 end
